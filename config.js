cfd2e06bad50ef8a489b6323408ef2308076735c
peachyluvu <85610587+peachyluvu@users.noreply.github.com>
2021-06-09 15:29:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Bảo bối à ~~!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
+    btnIntro: 'a yêu e',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
+    btnYes: 'Vẫn cứ là chiều em <333333',
+    btnNo: 'Không, a ghét em. Ble. đố a kiếm dc ai dễ xưn đa tài như e. 😉 :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: 'Hong bao h hết chiều chuộng em mô dù là khi cáu gắt. ',
+    mess: 'Em biết mà, bởi dì e là tục tưng của bảo bối TAnh 🥰 😘😘',
+    messDesc: 'Sáng mai e qua nhé ',
+    btnAccept: 'Oke luônggggg',
+    messLink: 'https://www.facebook.com/messages/t/100015539714875' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
